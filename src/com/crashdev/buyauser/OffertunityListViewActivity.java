f84e5e90408fcd6076bd42371a23b80be9fe0777
luis <luis.crmz@gmail.com>
2013-06-17 03:29:12 -0500

    agregando el backend app usuario

--- src/com/crashdev/buyauser/OffertunityListViewActivity.java
+++ src/com/crashdev/buyauser/OffertunityListViewActivity.java
@@ -1,10 +1,15 @@
 package com.crashdev.buyauser;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import android.view.View;
@@ -15,11 +20,29 @@ import android.widget.ListView;
 import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.json.jackson.JacksonFactory;
+
+
+import android.os.AsyncTask;
+import android.content.Context;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.json.jackson.JacksonFactory;
+import com.crashdev.buyauser.CloudEndpointUtils;
+import com.crashdev.buyauser.ofertaendpoint.Ofertaendpoint;
+import com.crashdev.buyauser.ofertaendpoint.model.CollectionResponseOferta;
+import com.crashdev.buyauser.ofertaendpoint.model.Oferta;
 
 public class OffertunityListViewActivity extends SherlockActivity {
 	
 	private ListView lv;
 	private OffertunityListviewAdapter listAdapter;
+	private CollectionResponseOferta resultQuery;
 	
 
 	@Override
@@ -36,42 +59,42 @@ public class OffertunityListViewActivity extends SherlockActivity {
 		oferta.setMainImage(R.drawable.example_1);
 		oferta.setOffertTitle("2x1 Toda la semana");
 		oferta.setAdvertiserName("Your company name here");
-		oferta.setOfferDescription("Esta semana por inauguarción...");
+		oferta.setOfferDescription("Esta semana por inauguarciï¿½n...");
 		listaDeOfertas.add(oferta);
 		
 		OffertunityCard oferta2 = new OffertunityCard();
 		oferta2.setMainImage(R.drawable.example_2);
 		oferta2.setOffertTitle("2x1 Toda la semana");
 		oferta2.setAdvertiserName("Liverpool");
-		oferta2.setOfferDescription("Esta semana por inauguarción...");
+		oferta2.setOfferDescription("Esta semana por inauguarciï¿½n...");
 		listaDeOfertas.add(oferta2);
 	
 		OffertunityCard oferta3 = new OffertunityCard();
 		oferta3.setMainImage(R.drawable.example_3);
 		oferta3.setOffertTitle("2x1 Toda la semana");
 		oferta3.setAdvertiserName("Your company name here");
-		oferta3.setOfferDescription("Esta semana por inauguarción...");
+		oferta3.setOfferDescription("Esta semana por inauguarciï¿½n...");
 		listaDeOfertas.add(oferta3);
 		
 		OffertunityCard oferta4 = new OffertunityCard();
 		oferta4.setMainImage(R.drawable.example_4);
 		oferta4.setOffertTitle("2x1 Toda la semana");
 		oferta4.setAdvertiserName("Your company name here");
-		oferta4.setOfferDescription("Esta semana por inauguarción...");
+		oferta4.setOfferDescription("Esta semana por inauguarciï¿½n...");
 		listaDeOfertas.add(oferta4);
 		
 		OffertunityCard oferta5 = new OffertunityCard();
 		oferta5.setMainImage(R.drawable.example_5);
 		oferta5.setOffertTitle("2x1 Toda la semana");
 		oferta5.setAdvertiserName("Your company name here");
-		oferta5.setOfferDescription("Esta semana por inauguarción...");
+		oferta5.setOfferDescription("Esta semana por inauguarciï¿½n...");
 		listaDeOfertas.add(oferta5);
 		
 		OffertunityCard oferta6 = new OffertunityCard();
 		oferta6.setMainImage(R.drawable.example_6);
 		oferta6.setOffertTitle("2x1 Toda la semana");
 		oferta6.setAdvertiserName("Your company name here");
-		oferta6.setOfferDescription("Esta semana por inauguarción...");
+		oferta6.setOfferDescription("Esta semana por inauguarciï¿½n...");
 		listaDeOfertas.add(oferta6);
 		
 		listAdapter = new OffertunityListviewAdapter( listaDeOfertas, getApplicationContext());
@@ -89,6 +112,8 @@ public class OffertunityListViewActivity extends SherlockActivity {
 			}
 			
 		});
+		
+		
 	}
 
 	@Override
@@ -102,11 +127,62 @@ public class OffertunityListViewActivity extends SherlockActivity {
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// TODO Auto-generated method stub
 		if(item.getItemId() == R.id.favoritos){
-			startActivity(new Intent(getApplicationContext(), OffertunitiesMapViewActivity.class));
+			//startActivity(new Intent(getApplicationContext(), OffertunitiesMapViewActivity.class));
+			new EndpointsTask().execute(getApplicationContext());
 		}else if(item.getItemId() == R.id.configuracion){
 			startActivity(new Intent(getApplicationContext(), OffertunitieDetailActivity.class));
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	
+	public class EndpointsTask extends AsyncTask<Context, Integer, Long> {
+		protected Long doInBackground(Context... contexts) {
+			
+			
+			Ofertaendpoint.Builder builder = new Ofertaendpoint.Builder(
+					AndroidHttp.newCompatibleTransport(), new GsonFactory(),
+					null);
+					//builder.setApplicationName("buyabeta");
+					Ofertaendpoint service = builder.build();
+					//System.out.println(service.getApplicationName());
+					
+					try {
+						System.out.println("Q pedo???");
+						resultQuery = service.listOferta().execute();
+						System.out.println(resultQuery);
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+			
+			/*Ofertaendpoint.Builder endpointBuilder = new Ofertaendpoint.Builder(
+					AndroidHttp.newCompatibleTransport(), new JacksonFactory(),
+					new HttpRequestInitializer() {
+						public void initialize(HttpRequest httpRequest) {
+						}
+					});
+			Ofertaendpoint endpoint = CloudEndpointUtils.updateBuilder(
+					endpointBuilder).build();
+			try {
+				Oferta note = new Oferta();
+				String noteID = new Date().toString();
+				note.setId(noteID);
+				note.setAdvertaicer("nota");
+				note.setTitulo("nota");
+				note.setCondiciones("nota");
+				note.setDescripcion("nota");
+				note.setVigencia("nota");
+				note.setImagen("aca url imagen");
+				Oferta result = endpoint.insertOferta(note).execute();
+				//resultQuery = endpoint.getOferta("name=1970-01-01").execute();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}*/
+			
+			return (long) 0;
+		}
 	}
 
 }
