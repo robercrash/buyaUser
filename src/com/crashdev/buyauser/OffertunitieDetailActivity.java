da10b5f1dcc0b2e1f1eb43d17f72860e5188011b
Oscar Alvarez <ozkar_ram@hotmail.com>
2013-06-16 20:04:04 -0600

    detail view

---
+++ src/com/crashdev/buyauser/OffertunitieDetailActivity.java
@@ -0,0 +1,23 @@
+package com.crashdev.buyauser;
+
+import com.actionbarsherlock.app.SherlockActivity;
+
+import android.os.Bundle;
+
+public class OffertunitieDetailActivity extends SherlockActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		setTheme(R.style.Theme_Sherlock_Light);
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_offertunitie_detail);
+	}
+	/*
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.offertunitie_detail, menu);
+		return true;
+	}*/
+
+}
